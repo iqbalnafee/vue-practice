50f3c61d8287dd6bd0dad818d7f5594770c5adc6
Nafees Iqbal <iqbalnafee@gmail.com>
2024-09-28 16:16:19 +0200

    v-for loop added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <h1>{{ name }}</h1>
   <p v-if="status === 'active'"> User is active </p>
   <p v-else>User is inactive</p>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
 
 <script>
@@ -10,6 +14,12 @@ export default{
     return{
       name: 'John Doe',
       status:'active',
+      tasks:[
+        'Task 1',
+        'Task 2',
+        'Task 3',
+      ],
+      link: "https://google.com",
     };
   }
 }
